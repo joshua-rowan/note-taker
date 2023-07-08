addff82d0e01eae10b367c67a41b409483bd1845
CardsClaw <joshr1979@gmail.com>
2023-07-08 12:01:18 -0500

    corrected missing /

--- routes/routes.js
+++ routes/routes.js
@@ -11,11 +11,11 @@ router.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
-router.get('api/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     //code to read the notes from JSON file
 });
 
-router.post('api/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     //code to receive and post new notes
 });
 
